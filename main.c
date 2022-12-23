5d1447bc11ff13e5ecd70f5ecf7c26e9725ad9d9
iblanco-s <iblanco-@student.42urduliz.com>
2022-12-23 13:13:17 +0100

    03?·_

--- main.c
+++ main.c
@@ -6,15 +6,19 @@
 /*   By: iblanco- <iblanco-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 16:54:40 by iblanco-          #+#    #+#             */
-/*   Updated: 2022/12/19 17:19:13 by iblanco-         ###   ########.fr       */
+/*   Updated: 2022/12/23 13:09:06 by iblanco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+// #include "push_swap.h"
 
 
-//A tener en cuenta: 
-int	main (int argc, char **argv)
-{
+// //A tener en cuenta: 
+//MAX INT Y MIN INT, vienen de una string, larrain hace que cuando necesite lso espacios necesrarios para el numero y si son los mismo que los maxminint los compara uno a uno de izquierda a derecha y si hay alguno mayor error
+
+// int	main (int argc, char **argv)
+// {
 	
-}
+// }
+//
+
